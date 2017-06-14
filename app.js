f0c1b6fd65218c8da1c79989b075886a9e942d84
funnbot <unconfigured@null>
2017-06-14 08:03:54 -0700

    Functioning pull data

--- app.js
+++ app.js
@@ -6,58 +6,73 @@ const port = 2001
 const auth = require('./auth.js')
 const r = require('rethinkdb')
 
+top()
 
+async function top() {
 
-app.use(express.static('public'))
+  let conn = await loadDB()
 
-app.set("port", port)
+  app.use(express.static('public'))
 
-router.use(function (req, res, next) {
-  console.log(req.url);
-  next();
-});
+  app.set("port", port)
 
-router.get("/", function (req, res) {
-  res.sendFile(path + "index.html");
-});
+  router.use(function (req, res, next) {
+    console.log(req.url);
+    next();
+  });
 
-router.get("/dashboard", function (req, res) {
-  res.sendFile(path + "dashboard.html");
-});
+  router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+  });
 
-app.use("/", router);
+  router.get("/dashboard", function (req, res) {
+    res.sendFile(path + "dashboard.html");
+  });
 
-app.use("*", function (req, res) {
-  res.sendFile(path + "404.html");
-});
+  //API
+  router.use('/database', function (req, res) {
+    if (req.method === "GET") {
+      let returnObj = {
+        error: true
+      }
+      let token = req.headers.authorization
+      r.db('Nitro').table('config').filter({
+        "data": {
+          "apitoken": token
+        }
+      }).run(conn, (err, data) => {
+        if (err) return res.send(JSON.stringify(returnObj))
+        data.toArray((err, array) => {
+          if (err) return res.send(JSON.stringify(returnObj))
+          if (array.length < 1) res.send(JSON.stringify(returnObj))
+          if (array.length === 1) res.send(JSON.stringify(array[0]))
+          if (array.length > 1) res.send(JSON.stringify(returnObj))
+        })
+      })
 
-//Api
-app.use('/api/1/database', function (req, res) {
-  if (req.method === "GET") {
-
-  }
-  if (req.method === "POST") {
-
-  }
-})
-
-app.listen(app.get("port"), function () {
-  console.log("Live at Port " + app.get("port"));
-});
-
-function loadDB() {
-
-  return new Promise((resolve, reject) => {
-
-    r.connect({
-      host: "localhost",
-      port: '28015',
-      db: "Nitro",
-      password: auth.rethink
-    }).run(conn => {
-
-    })
-
+    }
   })
+
+  app.use("/", router);
+
+  app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
+  });
+
+  app.listen(app.get("port"), function () {
+    console.log("Live at Port " + app.get("port"));
+  });
+
+}
+
+async function loadDB() {
+
+  let conn = await r.connect({
+    host: "localhost",
+    port: '28015',
+    db: "Nitro",
+    password: auth.rethink
+  })
+  return conn
 
 }
--- public/js/nitro.js
+++ public/js/nitro.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#load_db').click(function() {
-
-        
+    $('#load_db').click(function () {
+        Materialize.toast("Loading Database", 3000, "rounded blue")
+        var token = $('#api_key').val()
+        loadDB(token, (err, data) => {
+            if (err) return Materialize.toast("Invalid Token", 3000, "rounded red")
+            alert(JSON.stringify(data))
+        })
 
     })
 
 })
 
-function loadDB(token) {
-    
-    $.ajax()
+function loadDB(token, cb) {
+    $.ajax({
+        url: "/database",
+        method: "GET",
+        headers: {
+            Authorization: token
+        },
+        dataType: "json",
+        error: function (obj, err) {
+            cb(err, null)
+        },
+        success: function (data) {
+            cb(null, data)
+        }
+    })
 
 }
