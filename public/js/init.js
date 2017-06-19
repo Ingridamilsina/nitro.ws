992be7c9c9737561a373bcba2a826612e515d7f1
funnbot <unconfigured@null>
2017-06-18 19:51:41 -0700

    s

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+//Initialize Jquery
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
--- public/js/nitro.js
+++ public/js/nitro.js
@@ -314,17 +314,6 @@ function loadDB(token, cb) {
 
 }
 
-//Initialize Jquery
-(function($) {
-    $(function() {
-
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
 
 
 
